b6b475a00185f1c165267e2cdd0ceb2f23d3eb50
Daryl <dyk0203@naver.com>
2023-04-14 16:33:06 +0900

    ios SafeAreaView 적용(노치)

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
+import { Platform, SafeAreaView } from "react-native";
 import WebView from "react-native-webview";
 
 export default function App() {
-  return <WebView source={{ url: "https://kigox.vercel.app" }} />;
+  return Platform.OS === "ios" ? (
+    <SafeAreaView>
+      <WebView source={{ url: "https://kigox.vercel.app" }} />
+    </SafeAreaView>
+  ) : (
+    <WebView source={{ url: "https://kigox.vercel.app" }} />
+  );
 }
